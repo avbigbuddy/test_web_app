5314dac7923335827a3d46ab6d1e6dd85d244030
Argha Das <hdark6336@gmail.com>
2023-10-04 22:32:22 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const cors = require("cors")
+const express = require("express")
+const app = express()
+app.use(express.json());
+app.use(cors())
+
+app.use(express.static(__dirname));
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "index.html")
+})
+app.listen(8080, () => {
+})
